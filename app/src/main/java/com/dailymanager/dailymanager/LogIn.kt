4a4c6353751b0e93e7d373a470eca451ae95b9ec
dipankar108 <nathbd10@gmail.com>
2022-04-04 13:13:32 +0600

    log in page designed completed

--- app/src/main/java/com/dailymanager/dailymanager/LogIn.kt
+++ app/src/main/java/com/dailymanager/dailymanager/LogIn.kt
@@ -1,16 +1,53 @@
 package com.dailymanager.dailymanager
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.viewpager.widget.ViewPager
+import kotlinx.coroutines.*
 
-class LogIn : AppCompatActivity() {
+class LogIn : AppCompatActivity(), View.OnClickListener {
+
     private lateinit var viewpager: ViewPager
+    private lateinit var get_started_btn: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_log_in)
         viewpager = findViewById(R.id.vp_login_id)
+        get_started_btn = findViewById(R.id.btn_getStarted_id)
+
         val viewpageAdapter = ViewPagerAdapter()
         viewpager.adapter = viewpageAdapter
+        autoSlider(0)
+        get_started_btn.setOnClickListener(this)
+    }
+
+    private fun autoSlider(i: Int) {
+        var a = i + 1
+        if (i == 3) {
+            a = 0
+        }
+        GlobalScope.launch {
+            delay(3000)
+            withContext(Dispatchers.Main) {
+                viewpager.apply {
+                    currentItem = a
+                    true
+                }
+                autoSlider(a++)
+            }
+        }
+    }
+
+    override fun onClick(view: View?) {
+        when (view?.id) {
+            R.id.btn_getStarted_id -> {
+                startActivity(Intent(this@LogIn, MainActivity::class.java))
+                finish()
+            }
+
+        }
     }
 }
--- app/src/main/java/com/dailymanager/dailymanager/SplashScreen.kt
+++ app/src/main/java/com/dailymanager/dailymanager/SplashScreen.kt
@@ -1,5 +1,6 @@
 package com.dailymanager.dailymanager
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
@@ -11,10 +12,22 @@ class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
-        GlobalScope.launch {
-            delay(1000)
-            startActivity(Intent(this@SplashScreen, LogIn::class.java))
-            finish()
+        val sharedPref = getSharedPreferences("opened", Context.MODE_PRIVATE)
+        val value = sharedPref.getBoolean("isOpened", false)
+        if (value) {
+            GlobalScope.launch {
+                delay(1000)
+                startActivity(Intent(this@SplashScreen, MainActivity::class.java))
+                finish()
+            }
+        } else {
+            GlobalScope.launch {
+                delay(1000)
+                startActivity(Intent(this@SplashScreen, LogIn::class.java))
+                finish()
+            }
+            sharedPref.edit().putBoolean("isOpened", true).apply()
         }
+
     }
 }
