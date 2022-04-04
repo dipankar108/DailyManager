24470a9bdb6af54a9b881415ee1efed6392d19a1
dipankar108 <nathbd10@gmail.com>
2022-04-04 13:33:11 +0600

    bottomnav

---
+++ app/src/main/java/com/dailymanager/dailymanager/CreateTask.kt
@@ -0,0 +1,11 @@
+package com.dailymanager.dailymanager
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CreateTask : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_task)
+    }
+}
--- app/src/main/java/com/dailymanager/dailymanager/MainActivity.kt
+++ app/src/main/java/com/dailymanager/dailymanager/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.dailymanager.dailymanager
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    private lateinit var floatingActionButton: FloatingActionButton
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        floatingActionButton = findViewById(R.id.fab_mainActivity_id)
+        floatingActionButton.setOnClickListener(this)
+    }
+
+    override fun onClick(view: View?) {
+        when (view?.id) {
+            R.id.fab_mainActivity_id -> {
+                startActivity(Intent(this@MainActivity, CreateTask::class.java).apply {
+                    flags = Intent.FLAG_ACTIVITY_NEW_TASK
+                })
+            }
+        }
     }
 }
